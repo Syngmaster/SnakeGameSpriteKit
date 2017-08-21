ae1a99f6268c062e565c125d9a12fe775aebc59d
Syngmaster <max6361@mail.ru>
2017-08-21 14:07:38 +0100

    Initial Commit

---
+++ SnakeGameSK/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SnakeGameSK
+//
+//  Created by Syngmaster on 21/08/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ SnakeGameSK/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  SnakeGameSK
+//
+//  Created by Syngmaster on 21/08/2017.
+//  Copyright © 2017 Syngmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
